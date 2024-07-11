601115a9f6ba26183197ca26310b4b7fd117bc1b
luccacb16 <lucca.barberato@gmail.com>
2024-07-11 17:09:21 -0300

    add docker-compose

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-DATABASE_URL = "sqlite:///./test.db"
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
